fd644654a3cd6bb47ce170f4f3905c554ea8a7b1
CryptoGinge <68518401+CryptoGinge@users.noreply.github.com>
2021-11-29 18:26:46 +0000

    first changes

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 
 const whoami = document.getElementById("whoami")
+const contact = document.getElementById("contact")
+const socials = document.getElementById("socials")
+const blog = document.getElementById("blog")
 
 
 whoami.addEventListener('click',()=>{
-    const whoamibox = new WinBox({
+    new WinBox({
         title: '#whoami',
         width:'400px',
           height:'400px',
           background: "##01077C",
-          top:80,
-          right:150,
-          bottom:50,
-          left:340,
-          class:"text",
+          x: '100',
+          y: '100',
           html: "I am a cyber security professional with a background in law enforcement, I currently work in the information security team of a leading UK retailer responsible for thousands of users, assets and a vast array of technology and tools. Over the last decade I have built a great deal of experience around investigation management, incident response, intelligence management and cyber security. <br \><br \> I am an extremely passionate advocate for information security and feel I have a fantastic broad set of skills and flexibility that I can bring to an organisation. The skills I have honed over the past years compliment the cyber security field in particular my public facing roles have meant I have developed strong communications skills. This I believe blends well with my technical skills leading to me being a well rounded individual" 
         })
     new WinBox({
@@ -22,9 +22,20 @@ whoami.addEventListener('click',()=>{
         y: "center",
         height:'400px',
         background: "##01077C",
-        html: "Loads of skillz"
+        html: "SIEM (Splunk)<br \>Vulnerability scanning<br \>Penetration testing<br \>Web Development<br \>JavaScript/Node.js<br \>Cloud infrastructure & testing<br \>API Testing<br \>Investigation interviewing<br \>Forensic awareness<br \>Managing firewall rules"
     })
 })
 
 
-
+contact.addEventListener('click',()=>{
+    new WinBox({
+    title: 'Contact',
+    url: "https://nextapps-de.github.io/winbox/",
+    width:'400px',
+    height:'400px',
+    background: "##01077C",
+    x: '100',
+    y: '100',
+    html:'',
+    })
+})  

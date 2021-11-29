f0b65d12cd413dc54de8b4836a7cdf90ee4c4311
CryptoGinge <68518401+CryptoGinge@users.noreply.github.com>
2021-11-29 17:32:02 +0000

    Inital upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+const whoami = document.getElementById("whoami")
+
+
+whoami.addEventListener('click',()=>{
+    const whoamibox = new WinBox({
+        title: '#whoami',
+        width:'400px',
+          height:'400px',
+          background: "##01077C",
+          top:80,
+          right:150,
+          bottom:50,
+          left:340,
+          class:"text",
+          html: "I am a cyber security professional with a background in law enforcement, I currently work in the information security team of a leading UK retailer responsible for thousands of users, assets and a vast array of technology and tools. Over the last decade I have built a great deal of experience around investigation management, incident response, intelligence management and cyber security. <br \><br \> I am an extremely passionate advocate for information security and feel I have a fantastic broad set of skills and flexibility that I can bring to an organisation. The skills I have honed over the past years compliment the cyber security field in particular my public facing roles have meant I have developed strong communications skills. This I believe blends well with my technical skills leading to me being a well rounded individual" 
+        })
+    new WinBox({
+        title: 'Skills',
+        width:'400px',
+        x: "center",
+        y: "center",
+        height:'400px',
+        background: "##01077C",
+        html: "Loads of skillz"
+    })
+})
+
+
+
